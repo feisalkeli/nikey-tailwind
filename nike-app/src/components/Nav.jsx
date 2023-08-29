9a2cd2aae9d8504f537e8a184b69d9c9d6dce88c
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-08-29 08:33:21 +0300

    create navbar

--- nike-app/src/components/Nav.jsx
+++ nike-app/src/components/Nav.jsx
@@ -1,29 +1,38 @@
-import React from "react";
-import headerLogo from "../assets/images/header-logo.svg";
-import hamburger from "../assets/icons/hamburger.svg";
+import { hamburger } from "../assets/icons";
+import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
-    <>
-      <header className="padding-x py-8 absolute z-10 w-full">
-        <nav className="flex justify-between items-center max-container">
-          <a href="/">
-            <img src={headerLogo} alt="nike check" width={130} height={29} />
-          </a>
-          <ul className="flex-1 flex justify-items-center gap-1 max-lg:hidden">
-            <li className="list-none">
-              <a id="home">Home</a>
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img
+            src={headerLogo}
+            alt="logo"
+            width={129}
+            height={29}
+            className="m-0 w-[129px] h-[29px]"
+          />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
             </li>
-            <li>
-              <a id="home">Home</a>
-            </li>
-            <li>
-              <a id="home">Home</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
+          ))}
+        </ul>
+
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="hamburger icon" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
   );
 };
 
