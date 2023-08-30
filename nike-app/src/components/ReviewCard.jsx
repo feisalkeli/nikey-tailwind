da938abeb07a36df36cbbafbee42938b1bdb8230
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-08-30 10:59:18 +0300

    create subscribe section

--- nike-app/src/components/ReviewCard.jsx
+++ nike-app/src/components/ReviewCard.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const ReviewCard = () => {
-  return <div>ReviewCard</div>;
+const ReviewCard = ({ imgUrl, customerName, rating, feedback }) => {
+  return (
+    <div className="flex justify-center items-center flex-col">
+      {imgUrl && (
+        <img
+          src={imgUrl}
+          alt="img"
+          className="rounded-full object-cover w-[120px] h-[120px]"
+        />
+      )}
+      <h3 className=" max-w-sm font-bold text-coral-red text-2xl font-montserrat">
+        lorem
+      </h3>
+      <p className="mt-6 max-w-sm text-center info-text">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam
+        voluptates vitae perferendis saepe quaerat, obcaecati beatae ratione at
+        assumenda voluptas!{feedback}
+      </p>
+    </div>
+  );
 };
 
 export default ReviewCard;
--- nike-app/src/sections/Subscribe.jsx
+++ nike-app/src/sections/Subscribe.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import Button from "../components/Button";
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      className="max-container flex justify-between items-center max-lg:flex-col gap-10 "
+      id="contact-us"
+    >
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
+        Sign Up for <span className="text-coral-red">Updates</span> and
+        Newsletters
+      </h3>
+      <div className="flex max-sm:flex-col items-center lg:max-w-[40% ]  gap-5 sm:border sm:border-slate-gray rounded-full">
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <button className="bg-coral-red flex justify-center sm:flex-col  p-3 px-6 rounded-full tex-white sm:w-auto sm:">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Subscribe;
