a98b8f457539c2c6cce8ff039f639892456d9214
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-08-30 10:06:11 +0300

    create review card

--- nike-app/src/sections/CustomerReviews.jsx
+++ nike-app/src/sections/CustomerReviews.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { reviews } from "../constants";
+import ReviewCard from "../components/ReviewCard";
 
 const CustomerReviews = () => {
-  return <div>CustomerReviews</div>;
+  return (
+    <section className="max-container">
+      <h3 className="font-palanquin text-center font-bold text-4xl">
+        What Our <span className="text-coral-red">Customers </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit ratione
+        labore nobis ullam rerum, earum quaerat aspernatur facere perferendis.
+        Perspiciatis?
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review, index) => (
+          <ReviewCard
+            key={index}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feddback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default CustomerReviews;
--- nike-app/src/sections/SpecialOffer.jsx
+++ nike-app/src/sections/SpecialOffer.jsx
@@ -3,37 +3,41 @@ import { offer } from "../assets/images";
 
 const SpecialOffer = () => {
   return (
-    <section className=" flex flex-wrap max-container items-center gap-10 max-xl:flex-col-reverse">
-      <div className=" flex-1 flex-row ">
+    <section className=" flex justify-between items-center gap-10 max-xl:flex-col-reverse max-w-[1440]">
+      <div className=" flex-1 flex-row lg:p-40">
         <img
           src={offer}
           alt="offer_image"
-          width={173}
-          height={600}
-          className="w-full object-contain"
+          width={773}
+          height={687}
+          className=" object-contain items-center "
         />
-        <div className="flex flex-1 flex-col  ">
-          <h3 className="text-4xl leading-normal font-palanquin font-bold">
-            <span className="text-coral-red">Special</span> Offer
-          </h3>
-          <p className=" mt-5 font-bold leading-7 font-montserrat text-slate-gray info-text">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
-            sed ea aut repudiandae consectetur tenetur nemo culpa expedita
-            voluptates illum?
-          </p>
-          <p className=" mt-5 font-bold leading-7 font-montserrat text-slate-gray info-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-            dolorem nesciunt explicabo cupiditate illo qui commodi odit quo modi
-            voluptates.
-          </p>
-        </div>
+      </div>
+      <div className="flex flex-1 flex-col max-w-lg mr-0 lg:mr-36 ">
+        <h3 className="text-4xl leading-normal font-palanquin font-bold">
+          <span className="text-coral-red">Special</span> Offer
+        </h3>
+        <p className=" mt-5 font-bold leading-7 font-montserrat text-slate-gray info-text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
+          sed ea aut repudiandae consectetur tenetur nemo culpa expedita
+          voluptates illum?
+        </p>
+        <p className=" mt-5 font-bold leading-7 font-montserrat text-slate-gray info-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolorem
+          nesciunt explicabo cupiditate illo qui commodi odit quo modi
+          voluptates.
+        </p>
         <div className="mt-11 flex items-center flex-wrap gap-4">
-          <button className="rounded-full bg-coral-red p-3 px-6 text-white">
-            Shop Now
-          </button>
-          <button className="rounded-full  p-3 px-6 text-slate-gray border-slate-gray hover:bg-coral-red hover:text-white">
-            Learn More
-          </button>
+          <div>
+            <button className="rounded-full bg-coral-red p-3 px-6 text-white">
+              Shop Now
+            </button>
+          </div>
+          <div>
+            <button className="rounded-full  p-3 px-6 text-slate-gray border-slate-gray hover:bg-coral-red hover:text-white">
+              Learn More
+            </button>
+          </div>
         </div>
       </div>
     </section>
